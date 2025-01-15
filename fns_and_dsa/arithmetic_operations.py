183db2231744ff3e9c9be738415d6af96453d695
gamisi <gamisi.ga@gmail.com>
2025-01-16 01:17:39 +0300

    updated arithmetic functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,3 +11,13 @@ def perform_operation(num1,num2,operation):
         return num1 / num2
     else:
         return "Error: Invalid operation. Please choose 'add', 'subtract', 'multiply', or 'divide'."
+
+def main():
+    num1 = float(input("Enter the first number:"))
+    num2 = float(input("Enter the second nuber:"))
+    operation =(input("Enter the operation ('add', 'subtract', 'multiply', or 'divide'))"))
+    result = perform_operation(num1,num2,operation)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
