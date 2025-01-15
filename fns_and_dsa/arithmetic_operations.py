1c7eb6388ac4112c1e9da814f3edc7d729337946
gamisi <gamisi.ga@gmail.com>
2025-01-16 01:32:05 +0300

    updated  functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,17 +8,6 @@ def perform_operation(num1,num2,operation):
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero is not allowed"
-        return num1 / num2
+        return num1/num2
     else:
         return "Error: Invalid operation. Please choose 'add', 'subtract', 'multiply', or 'divide'."
-"""
-def main():
-    num1 = float(input("Enter the first number:"))
-    num2 = float(input("Enter the second nuber:"))
-    operation =(input("Enter the operation (add,subtract,multiply,divide): )").strip().lower())
-    result = perform_operation(num1,num2,operation)
-    print(f"Result: {result}")
-
-if __name__ == "__main__":
-    main()
-"""
