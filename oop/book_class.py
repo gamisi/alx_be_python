9d651e59c657cd2c7b111a815b8bd48e99dd4449
gamisi <gamisi.ga@gmail.com>
2025-01-20 21:33:09 +0300

    added book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,14 +4,17 @@ class Book:
         self.author = author
         self.year = year
 
-        print(f"Book '{self.title}' by {self.author} created.")
+        #print(f"{self.title} by {self.author} created.")
     
     def __del__(self):
+        # Destructor prints a message when the object is deleted
         print(f"Deleting {self.title}")
-    
+
     def __str__(self):
+        # __str__ provides a user-friendly string representation
         return f"{self.title} by {self.author}, published in {self.year}"
-    
+
     def __repr__(self):
+        # __repr__ provides a string that could recreate the Book instance
         return f"Book('{self.title}', '{self.author}', {self.year})"
         
