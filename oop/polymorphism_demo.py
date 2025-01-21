4b7440cc8971ae407253c89ac00b89707ced5da0
gamisi <gamisi.ga@gmail.com>
2025-01-21 10:31:46 +0300

    added polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,7 +4,7 @@ class Shape():
     def area(self):
         raise NotImplementedError("Subclass must implement abstract method")
 
-class Rectangle():
+class Rectangle(Shape):
     def __init__(self, length, width):
         self.length = length
         self.width = width
@@ -12,7 +12,7 @@ class Rectangle():
     def area(self):
         return self.length*self.width
 
-class Circle():
+class Circle(Shape):
     def __init__(self, radius):
             self.radius = radius
     
